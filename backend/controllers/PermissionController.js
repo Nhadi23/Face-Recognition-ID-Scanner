1d9ed2c8f78ddea9bcd951f13a4416c9e048347f
cardinaldeacre <cardinaldeacre@gmail.com>
2025-12-22 20:34:33 +0700

    add new controller

--- backend/controllers/PermissionController.js
+++ backend/controllers/PermissionController.js
@@ -1,51 +1,70 @@
 const express = require('express');
 const router = express.Router();
-router.get('/', async (req, res) => {
-    try {
-      return res.status(200).json();
-    } catch (error) {
-      console.error(error);
-      return res.status(500).json({message: 'Error server'});
-    }
+const PermissionService = require('../services/PermissionService');
+const { authorizeRole, authMiddleware } = require('../middleware/auth');
+
+router.get('/my-history', authMiddleware, authorizeRole('student'), async (req, res) => {
+  try {
+    const history = await PermissionService.getActivePermission(req.user.id);
+    return res.status(200).json(history);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'Error server' });
+  }
 });
 
-router.post('/', async (req, res) => {
-    try {
-      res.status(201).json({message: ''});
-    } catch (error) {
-      if (error.code === '23505') return res.status(409).json({message: ''});
-  
-      console.error(error);
-      res.status(500).json({message: 'Error server'});
+router.post('/request', authMiddleware, authorizeRole('student'), async (req, res) => {
+  try {
+    const { reason, start_time, end_time } = req.body
+
+    if (!reason || !start_time || !end_time) {
+      return res.status(400).json({ message: 'All fields are required' });
     }
+
+    if (new Date(start_time) >= new Date(end_time)) {
+      return res.status(400).json({ message: 'Start time must be before end time' });
+    }
+
+    const newRequest = await PermissionService.create(req.user.id, { reason, start_time, end_time });
+    res.status(201).json({ message: 'Permintaan berhasil dibuat', permission: newRequest });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error server' });
+  }
 });
 
-router.put('/:id', async (req, res) => {
-  const {id} = req.params;
-	const updatedData = req.body;
+router.get('/admin/all', authMiddleware, authorizeRole('admin'), async (req, res) => {
+  try {
+    const request = await PermissionService.getAll();
+    return res.status(200).json(request);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'Error server' });
+  }
+});
 
-  if (Object.keys(updatedData).length === 0) {
-    return res.status(400).json({message: 'Data yang akan diupdate tidak boleh kosong'});
+router.patch('/admin/status/:id', authMiddleware, authorizeRole('admin'), async (req, res) => {
+  const { id } = req.params;
+  const { status } = req.body;
+
+  if (!['accepted', 'denied'].includes(status)) {
+    return res.status(400).json({ message: 'Status tidak valid' });
   }
 
   try {
-    const payload = {};
-    const itemId = parseInt(id);
+    const updatedPermission = await PermissionService.updateStatus(id, status);
 
-    const updateItem = await Service.update(itemId, payload);
-
-    if (!updateItem) {
-      return res.status(404).json({message: 'Item tidak ditemukan'});
+    if (!updatedPermission) {
+      return res.status(404).json({ message: 'Data perizinan tidak ditemukan' });
     }
 
-    res.status(200).json({message: 'Item berhasil diedit', item: updateItem});
+    res.status(200).json({
+      message: `Izin berhasil di-${status}`,
+      data: updatedPermission
+    });
   } catch (error) {
-    if (error.code === '23505') {
-      return res.status(409).json({message: 'item sudah digunakan.'});
-    }
-
     console.error(error);
-    res.status(500).json({message: 'Server error'});
+    res.status(500).json({ message: 'Server error' });
   }
 });
 
@@ -56,19 +75,19 @@ router.delete('/:id', async (req, res) => {
     const itemId = parseInt(id);
 
     if (isNaN(itemId)) {
-      return res.status(400).json({message: 'ID item tidak valid'});
+      return res.status(400).json({ message: 'ID item tidak valid' });
     }
 
     const deletedCount = await Service.delete(itemId);
 
     if (deletedCount === 0) {
-      return res.status(404).json({message: 'Item tidak ditemukan'});
+      return res.status(404).json({ message: 'Item tidak ditemukan' });
     }
-      
-    return res.status(200).json({message: 'Item berhasil dihapus'});
+
+    return res.status(200).json({ message: 'Item berhasil dihapus' });
   } catch (error) {
     console.error(error);
-    res.status(500).json({message: 'Server error'});
+    res.status(500).json({ message: 'Server error' });
   }
 });
 
